20d1ebec4f95a80f1c180b3ba32c5c6d4e8ae255
seafood <mail.seafood@proton.me>
2024-03-25 00:58:01 +0300

    Refactor database configuration and add database connection validation

--- db.js
+++ db.js
@@ -1,5 +1,12 @@
-const {Sequelize} = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 module.exports = new Sequelize(
-  
-)
+  process.env.DB_NAME, // Database name
+  process.env.DB_USER, // Database user
+  process.env.DB_PASSWORD, // Database password
+  {
+    dialect: 'postgres', // Database dialect
+    host: process.env.DB_HOST, // Database host
+    port: process.env.DB_PORT, // Database port
+  }
+);
