3fde747accbe055b96d6cd6ebd0a9de4a531db77
seafood <mail.seafood@proton.me>
2024-03-26 14:13:40 +0300

    Add checkRole middleware to deviceRouter.js

--- routes/deviceRouter.js
+++ routes/deviceRouter.js
@@ -1,6 +1,7 @@
 const Router = require('express');
 const router = new Router();
 const DeviceController = require('../controllers/deviceController');
+const checkRole = require('../middleware/checkRoleMiddleware');
 
 router.post('/', checkRole('ADMIN'), DeviceController.create);
 router.get('/', DeviceController.getAll);
