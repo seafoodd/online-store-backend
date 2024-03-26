d8714f30fc539d275d505871a04230fc436b9259
seafood <mail.seafood@proton.me>
2024-03-26 14:48:52 +0300

    Add authentication middleware to getOne route

--- routes/deviceRouter.js
+++ routes/deviceRouter.js
@@ -2,9 +2,10 @@ const Router = require('express');
 const router = new Router();
 const DeviceController = require('../controllers/deviceController');
 const checkRole = require('../middleware/checkRoleMiddleware');
+const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/', checkRole('ADMIN'), DeviceController.create);
 router.get('/', DeviceController.getAll);
-router.get('/:id', DeviceController.getOne);
+router.get('/:id', authMiddleware, DeviceController.getOne);
 
 module.exports = router;
